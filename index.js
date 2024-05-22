c95fde83c50391ff6d93d48f48d7d253418d4494
Satyam Jha <satyammjha0@gmail.com>
2024-05-22 16:03:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ const nft_collection = [];
 
 function mintNFT(name, owner, date) {
     const NFT = {
-       "name" : name,
-       "owner" : owner,
-       "minted on" : date
+       "name": name,
+       "owner": owner,
+       "minted on": date
     }
     nft_collection.push(NFT);
 }
 
 
 function listNFTs() {
-    console.log("List of the minthed NFT's:")
+    console.log("List of the minted NFT's:")
     let i;
     let j = nft_collection.length;
     for (i = 0; i < j; i++) {
@@ -26,7 +26,7 @@ function getTotalSupply() {
 
 mintNFT("Satyam's NFT", "Satyam Jha", "22/05/24");
 mintNFT("Football's NFT", "Messi", "22/05/24");
-mintNFT("Cricket's NFT", "Virat Kohli", "22/05/24);
+mintNFT("Cricket's NFT", "Virat Kohli", "22/05/24");
 
 listNFTs();
 getTotalSupply();
